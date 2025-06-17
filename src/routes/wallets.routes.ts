7f4058856c6e9a4d0e60b4e2be2a6a383ae5c17c
Josue19-08 <josuemarin2009@hotmail.com>
2025-06-16 20:04:37 -0600

    chore: connect wallet controller to router

--- src/routes/wallets.routes.ts
+++ src/routes/wallets.routes.ts
@@ -2,9 +2,7 @@ import { Router } from 'express';
 const { createWallet } = require('../controllers/wallet.controller');
 
 const router = Router();
-console.log('[router] Wallet routes loaded');
 
 router.post('/create', createWallet);
-console.log('[router] Wallet routes loaded');
 
 export default router;
