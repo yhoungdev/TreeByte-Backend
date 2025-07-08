10240a37bea049ae42dbcfe20d2b596acdd2e024
ahuja-shivam <ahujas1203@gmail.com>
2025-07-08 08:07:09 +0530

    Create POST /buy-token endpoint to handle project token purchases

--- src/scripts/users.sql
+++ src/scripts/users.sql
@@ -7,17 +7,3 @@ CREATE TABLE IF NOT EXISTS users (
   created_at TIMESTAMPTZ DEFAULT NOW(),
   updated_at TIMESTAMPTZ DEFAULT NOW()
 );
-
-
-CREATE TABLE IF NOT EXISTS projects (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  name TEXT NOT NULL,
-  description TEXT NOT NULL,
-  location TEXT NOT NULL,
-  photo_url TEXT NOT NULL,
-  impact TEXT NOT NULL,
-  asset_code TEXT NOT NULL,
-  supply TEXT NOT NULL,
-  created_at TIMESTAMPTZ DEFAULT NOW(),
-  updated_at TIMESTAMPTZ DEFAULT NOW()
-);
