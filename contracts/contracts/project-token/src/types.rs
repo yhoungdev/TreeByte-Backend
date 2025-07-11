9f92bf06e28b0c7fa876acc43795dc6e0b66673f
Josue19-08 <josuemarin2009@hotmail.com>
2025-07-10 20:40:59 -0600

    feat(project-token): define DataKey enum for contract storage keys

--- contracts/contracts/project-token/src/types.rs
+++ contracts/contracts/project-token/src/types.rs
@@ -2,8 +2,10 @@ use soroban_sdk::{contracttype, Address};
 
 #[derive(Clone)]
 #[contracttype]
-
 pub enum DataKey {
-    Balance(Address),
-    RemainingSupply,
+    Balance(Address),       // Stores individual user balances
+    RemainingSupply,        // Remaining tokens available for sale
+    ProjectName,            // Project name (string)
+    ProjectId,              // Project ID (string, can adapt to u64 if needed)
+    IssuerAddress,          // Project issuer's address
 }
